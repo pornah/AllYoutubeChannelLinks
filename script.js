67af2ebfb9f71933f8305b92ce954461718fe23c
WesleyCSJ <wesleyjr10@gmail.com>
2015-12-27 17:45:17 -0300

    Fixing some things
    All right now!

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var list  = document.getElementsByClassName('yt-lockup-title');
-console.log(a);
 for each(var link in list){
   var content = document.createTextNode('https://www.youtube.com' + link.firstChild.getAttribute('href'));
   var p = document.createElement('p');
